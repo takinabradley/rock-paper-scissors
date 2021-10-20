ac0a9e81759ad0ac4a12da661d5e8930bf35ee9b
takinabradley <bradfinney98@gmail.com>
2021-10-20 19:12:34 -0400

    fixed whatever happened with the merge conflict- I guess I'll learn how to resolve those properly later

--- script.js
+++ script.js
@@ -24,25 +24,6 @@ function computerPlay() {
 
 
 function playRound(e) {
-<<<<<<< HEAD
-  let playerWin;
-  
-	let computerSelection = computerPlay();
-  roundResults.textContent = `Computer Picked: ${computerSelection}`;
-  console.log(`Computer picked: ${computerSelection}`);
-  
-  let playerSelection = e.target.classList.toString();
-  
-  if (playerSelection === computerSelection) {
-    //console.log(`You picked: ${playerSelection}`);
-    //console.log(`Computer picked: ${computerSelection}`);
-    playerWin;
-    //console.log(playerWin);
-    return playerWin;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      //console.log(`You picked: ${playerSelection}`);
-      //console.log(`Computer picked: ${computerSelection}`);
-=======
   const computerSelection = computerPlay();
   const playerSelection = e.target.classList.toString();
   let playerWin;
@@ -54,88 +35,26 @@ function playRound(e) {
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
       console.log(`Player Wins! Computer picked: ${computerSelection}`);
       document.querySelector('.roundOutput').textContent = `Player Wins! Computer picked: ${computerSelection}`;
->>>>>>> new-logic
       playerWin = 1;
-      //console.log(playerWin);
       return playerWin;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-<<<<<<< HEAD
-      //console.log(`You picked: ${playerSelection}`);
-      //console.log(`Computer picked: ${computerSelection}`);
-=======
       console.log(`Player Wins! Computer picked: ${computerSelection}`);
       document.querySelector('.roundOutput').textContent = `Player Wins! Computer picked: ${computerSelection}`;
->>>>>>> new-logic
       playerWin = 1;
-      //console.log(playerWin);
       return playerWin;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-<<<<<<< HEAD
-      //console.log(`You picked: ${playerSelection}`);
-      //console.log(`Computer picked: ${computerSelection}`);
-=======
       console.log(`Player Wins! Computer picked: ${computerSelection}`);
       document.querySelector('.roundOutput').textContent = `Player Wins! Computer picked: ${computerSelection}`;
->>>>>>> new-logic
       playerWin = 1;
-      //console.log(playerWin);
       return playerWin;
   } else {
-<<<<<<< HEAD
-      //console.log(`You picked: ${playerSelection}`);
-      //console.log(`Computer picked: ${computerSelection}`);
-=======
       console.log(`Computer Wins! Computer picked: ${computerSelection}`);
       document.querySelector('.roundOutput').textContent = `Player Wins! Computer picked: ${computerSelection}`;
->>>>>>> new-logic
       playerWin = 0;
-      //console.log(playerWin);
       return playerWin;
   }
 }
 /*finds computerSelection, creates a variable for playerSelection and 
-<<<<<<< HEAD
-	prompts the player for the value. Decides whether the player wins or 
-	loses, and assigns 1 (win) or 0 (lose) to playerWin.*/
-	
-function game(playerWin) {
-
-    if (playerWin === 1) {
-      playerWinCount++;
-      console.log(`Player Wins! Player: ${playerWinCount} Computer: ${computerWinCount}`);
-      scoreCard.textContent = `Score: ${playerWinCount} : ${computerWinCount}`;
-    } else if (playerWin === 0) {
-      computerWinCount++;
-      console.log(`Computer Wins! Player: ${playerWinCount} Computer: ${computerWinCount}`);
-      scoreCard.textContent = `Score: ${playerWinCount} : ${computerWinCount}`;
-    } else {
-      console.log(`Draw! Player: ${playerWinCount} Computer: ${computerWinCount}`);
-      scoreCard.textContent = `Score: ${playerWinCount} : ${computerWinCount}`;
-    }
-    
-    if (roundCounter === 5) {
-      if (playerWinCount === computerWinCount) {
-        roundCounter = 0;
-        playerWinCount = 0;
-        computerWinCount = 0;
-        gameResults.textContent = "The Game Ends In A Draw!";
-        return console.log("The Game Ends In A Draw!");
-      } else if (playerWinCount > computerWinCount) {
-        roundCounter = 0;
-        playerWinCount = 0;
-        computerWinCount = 0;
-        gameResults.textContent = "Player Wins!";
-        return console.log("Player Wins!");
-      } else {
-        roundCounter = 0;
-        playerWinCount = 0;
-        computerWinCount = 0;
-        gameResults.textContent = "Computer Wins!";
-        return console.log("Computer Wins!");
-      }
-    }
-    
-=======
   prompts the player for the value. Decides whether the player wins or 
   loses, and assigns 1 (win) or 0 (lose) to playerWin.*/
   
@@ -193,54 +112,13 @@ function game () {
       //Next step is creating and pushing DOM outputs to the browser.
     });
   });
->>>>>>> new-logic
 }
 /*Passes the class of the button pressed to playRound() on every click. so it can determine the playerSelection.
   It then saves the value of playerWin from playRound so the game() function can use it.*/
 
 /*Creates playerWinCount and computerWinCount. Plays a five-round game that 
-<<<<<<< HEAD
-	keeps score and adds every playerWin = 1 to playerWinCount and 
-	playerWin = 0 to computerWinCount from playRound() on each round. Then
-	reports a winner or loser at the end of the game.*/
-
-let playerWinCount = 0;
-let computerWinCount = 0;
-let playerWin;
-let roundCounter = 0;
-
-const output = document.querySelector('.output');
-
-let scoreCard = document.createElement('p');
-output.appendChild(scoreCard);
-
-let roundResults = document.createElement('p');
-output.appendChild(roundResults);
-
-let gameResults = document.createElement('p');
-output.appendChild(gameResults);
-
-function playerPlay () {
-const buttons = document.querySelectorAll('button');
-buttons.forEach( (button) => {
-  button.addEventListener('click', (e) => {
-    roundCounter += 1;
-    if (gameResults.textContent !== '') gameResults.textContent = '';
-    playerWin = game(playRound(e));
-    
-    
-    //scoreCard.textContent = `Score: ${playerWinCount} : ${computerWinCount}`;
-
-    //console.log(roundCounter);
-  });
-});
-}
-
-playerPlay();
-=======
   keeps score and adds every playerWin = 1 to playerWinCount and 
   playerWin = 0 to computerWinCount from playRound() on each round. Then
   reports a winner or loser at the end of the game.*/
   
 game();
->>>>>>> new-logic
